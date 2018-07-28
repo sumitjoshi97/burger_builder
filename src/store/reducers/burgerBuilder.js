f652ea5025926b801a441f19f234a681cec8685a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-28 22:37:33 +0530

    added new state

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,12 +1,11 @@
 import * as actionTypes from '../actions/actionTypes';
-import {
-    updateObject
-} from '../utility'
+import { updateObject } from '../utility'
 
 const initialState = {
     ingredients: null,
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
 }
 
 const INGREDIENT_PRICES = {
@@ -16,53 +15,64 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 };
 
+const addIngredient = (state, action) => {
+    const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+    }
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    }
+    return updateObject(state, updatedState);
+}
+
+const removeIngredients = (state, action) => {
+    const updatedIng = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+    }
+    const updatedIngs = updateObject(state.ingredients, updatedIng)
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    }
+    return updateObject(state, updatedSt);
+}
+
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: 4,
+        error: false,
+        building: false
+    })
+}
+
+const fetchingredientsFailed = (state, action) => {
+    return updateObject(state, {error: true})
+}
+
 const burgerBuilderReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-            }
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedState);
-            // return {
-            //     ...state,
-            //     ...updatedIngredients,
-
-            // }
+        case actionTypes.ADD_INGREDIENT: 
+            return addIngredient(state, action);
 
         case actionTypes.REMOVE_INGREDIENT:
-            const updatedIng = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-            }
-            const updatedIngs = updateObject(state.ingredients, updatedIng)
-            const updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updateObject(state, updatedSt);
+           return removeIngredients(state, action);
 
         case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                totalPrice: 4,
-                error: false
-            }
+            return setIngredients(state, action);
+            
 
         case actionTypes.FETCH_INGREDIENT_FAILED:
-            return {
-                ...state,
-                error: true
-            }
+            return fetchingredientsFailed(state, action);
         default:
             return state
     }
